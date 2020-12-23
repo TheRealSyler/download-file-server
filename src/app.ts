e1b7cf4a2af7c35cbd38b7ae1e037e59a6467f56
TheRealSyler <hileo77@gmail.com>
2020-12-23 18:53:57 +0100

    add ability to upload files

--- src/app.ts
+++ src/app.ts
@@ -1,41 +1,53 @@
 import express = require('express');
 import { Response } from 'express';
 import { Server } from 'http';
-import { normalize, resolve } from 'path';
-import { createReadStream, promises, readdir, readFileSync } from 'fs';
-const port = 4000;
+import { join, resolve } from 'path';
+import { createReadStream, createWriteStream, promises, existsSync, mkdirSync} from 'fs';
+import busboy from 'connect-busboy';
 
+const port = 4000;
+const ip = '192.168.7.131'
+const uploadPath = resolve(process.cwd(),'assets')
+ensureDir(uploadPath); 
+ 
 const app = express();
 const server = new Server(app);
+
+app.use(busboy({
+  highWaterMark: 2 * 1024 * 1024,
+}));
+
 app.get('/', async (req, res) => {
   console.log('get: /', req.ip)
-  res.send(`<!DOCTYPE html>
-<html lang="en">
+  const css = (await promises.readFile('./public/index.css')).toString()
+  const html = (await promises.readFile('./public/index.html')).toString()
 
-<head>
-  <meta charset="UTF-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <meta http-equiv="X-UA-Compatible" content="ie=edge">
-  <title>Downloads</title>
-  <style>${(await promises.readFile('./public/index.css')).toString()}</style>
-</head>
-  
-<body>
-  <div class="header" style="text-align: center">click to download</div>
-  ${await genCards()}
-  <script>
-    function Download(path) {
-      window.open(\`/download/\${path}\`)
-      console.log('DOWNLOAD', path)
-    }
-  </script>
-  </body>
-</html>`)
-
+  res.send(html.replace('--CARDS--', await genCards()).replace('--STYLE--', `<style>${css}</style>`))
 });
-
+ 
 app.get('/download/:name', (req, res) => {
   Download(req.ip,res, req.params.name);
+});
+
+
+app.post("/upload", (req, res) => {
+
+  req.pipe(req.busboy); 
+
+  req.busboy.on('file', (fieldName, file, filename) => {
+    console.log(`Upload of '${filename}' started`);
+
+    
+    const fsStream = createWriteStream(join(uploadPath, filename));
+    file.pipe(fsStream);
+
+
+    fsStream.on('close', () => {
+      console.log(`Upload of '${filename}' finished`);
+      res.redirect('back');
+    });
+  });
+
 });
 
 async function genCards() {
@@ -43,16 +55,17 @@ async function genCards() {
   const files = await promises.readdir('./assets')
   for (let i = 0; i < files.length; i++) {
     const file = files[i];
-    out += `<div class="container" onclick="Download('${file}')">
+    out += `<div class="container" onclick="Download('${file}')" title="Click to Download">
   <h2>${file}</h2>
 </div>`
   }
   return out
 }
-
-server.listen(port, '192.168.7.131', () => {
-  console.log('Listening... ', `http://192.168.7.131:${port}`);
+ 
+server.listen(port,ip , () => {
+  console.log('Listening... ', `http://${ip}:${port}`);
 });
+
 function Download(ip: string,res: Response, file: string) {
   console.log('Download:', file, `(${ip})`, new Date().toLocaleTimeString());
   // ONLY FILES WORK, FOLDERS DO NOT GET DOWNLOADED
@@ -62,4 +75,11 @@ function Download(ip: string,res: Response, file: string) {
   a.addListener('close', () => {
     console.log('Downloaded:', file, `(${ip})`, new Date().toLocaleTimeString());
   })
+}
+
+function ensureDir(dir: string) {
+  const exits = existsSync(dir)
+  if (!exits) {
+    mkdirSync(dir)
+  }
 }
