4233fa59d9936ad192ad4cc4b34b7d20e114f1b5
TheRealSyler <hileo77@gmail.com>
2020-12-23 19:20:43 +0100

    add .env

--- src/app.ts
+++ src/app.ts
@@ -2,14 +2,17 @@ import express = require('express');
 import { Response } from 'express';
 import { Server } from 'http';
 import { join, resolve } from 'path';
-import { createReadStream, createWriteStream, promises, existsSync, mkdirSync} from 'fs';
+import { createReadStream, createWriteStream, promises, existsSync, mkdirSync } from 'fs';
 import busboy from 'connect-busboy';
+import { config } from 'dotenv'
 
-const port = 4000;
-const ip = '192.168.7.131'
-const uploadPath = resolve(process.cwd(),'assets')
-ensureDir(uploadPath); 
- 
+config()
+
+const PORT = process.env.PORT || 5000;
+const IP = process.env.IP || '192.168.7.131'
+const uploadPath = resolve(process.cwd(), 'assets')
+ensureDir(uploadPath);
+
 const app = express();
 const server = new Server(app);
 
@@ -24,20 +27,20 @@ app.get('/', async (req, res) => {
 
   res.send(html.replace('--CARDS--', await genCards()).replace('--STYLE--', `<style>${css}</style>`))
 });
- 
+
 app.get('/download/:name', (req, res) => {
-  Download(req.ip,res, req.params.name);
+  Download(req.ip, res, req.params.name);
 });
 
 
 app.post("/upload", (req, res) => {
 
-  req.pipe(req.busboy); 
+  req.pipe(req.busboy);
 
   req.busboy.on('file', (fieldName, file, filename) => {
     console.log(`Upload of '${filename}' started`);
 
-    
+
     const fsStream = createWriteStream(join(uploadPath, filename));
     file.pipe(fsStream);
 
@@ -61,15 +64,15 @@ async function genCards() {
   }
   return out
 }
- 
-server.listen(port,ip , () => {
-  console.log('Listening... ', `http://${ip}:${port}`);
+
+server.listen(PORT, IP as any, () => {
+  console.log('Listening... ', `http://${IP}:${PORT}`);
 });
 
-function Download(ip: string,res: Response, file: string) {
+function Download(ip: string, res: Response, file: string) {
   console.log('Download:', file, `(${ip})`, new Date().toLocaleTimeString());
   // ONLY FILES WORK, FOLDERS DO NOT GET DOWNLOADED
-  const files = createReadStream(resolve(process.cwd(),'assets', file));
+  const files = createReadStream(resolve(process.cwd(), 'assets', file));
   res.writeHead(200, { 'Content-disposition': `attachment; filename=${file}` }); //here you can specify file name
   const a = files.pipe(res)
   a.addListener('close', () => {
